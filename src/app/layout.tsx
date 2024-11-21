ef5e7bb78e51115cdea2051ee8e12907758000a6
jinyoung <wlsud801@gmail.com>
2024-11-21 22:45:53 +0900

    Feat: pixel 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,34 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                {/* Meta Pixel Code */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(f,b,e,v,n,t,s){
+                                if(f.fbq)return;n=f.fbq=function(){
+                                n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
+                            };
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '1083651480221454');
+                            fbq('track', 'PageView');
+                        `,
+                    }}
+                ></script>
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=1083651480221454&ev=PageView&noscript=1"
+                    />
+                </noscript>
+                {/* End Meta Pixel Code */}
+            </head>
             <body className={`${suit.variable} font-suit`}>
                 <main className="pc:w-[500px] mob:w-full border mx-auto bg-bl100 overflow-x-hidden">{children}</main>
             </body>
